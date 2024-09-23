eda2122079bcb94ed80f8fac60cfe3d36de4eab2
FelixTheuermann <felix.hoelfont.hobe.coa@gmail.com>
2024-09-23 13:07:26 +0200

    ..

--- 00_GEttingStarted/FirstProgram/Program.cs
+++ 00_GEttingStarted/FirstProgram/Program.cs
@@ -7,15 +7,21 @@ class Program
         int number = 0;
         int number2 = 0;
 
+
         number = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < 10; i++) {
         number2 = Convert.ToInt32(Console.ReadLine());
         
         if(number == number2) {
             Console.WriteLine($"You are right");
+            break;
         } else if(number < number2) {
             Console.WriteLine($"Your number is to high");
         } else {
             Console.WriteLine($"Your number is to low");
         }
+    }
+
+       
     }
 }
